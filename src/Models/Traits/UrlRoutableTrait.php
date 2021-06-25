012bd8a245fb71a3e214cb853026de3bb240bfdc
Rik Zwarthoff <zwarthoffr@gmail.com>
2021-06-25 16:05:15 +0200

    Fixes for urlroutabletrait

--- src/Models/Traits/UrlRoutableTrait.php
+++ src/Models/Traits/UrlRoutableTrait.php
@@ -1,10 +1,9 @@
 <?php
 
-
 namespace Flooris\ErgonodeApi\Models\Traits;
 
-
-use Flooris\ErgonodeApi\Models\Model;
+use Flooris\ErgonodeApi\Models\Contracts\Model;
+use Flooris\ErgonodeApi\Models\Contracts\ChildModel;
 
 trait UrlRoutableTrait
 {
@@ -18,9 +17,11 @@ trait UrlRoutableTrait
         return 'id';
     }
 
-    public function resolveRouteBinding($value, $field = null): ?Model
+    public function resolveRouteBinding($value, $field = null): null|Model|ChildModel
     {
-        $this->firstWhere($field ?? $this->getRouteKeyName(), $value);
+        $routeKeyName = $this->getRouteKeyName();
+
+        return $routeKeyName === 'id' ? $this->find($value) : $this->firstWhere($field ?? $routeKeyName, $value);
     }
 
     public function resolveChildRouteBinding($childType, $value, $field)
