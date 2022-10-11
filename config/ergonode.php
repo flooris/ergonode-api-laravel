10abaaa280a810939d1deb524caf9352952b9470
Florian Kwakkenbos <f.kwakkenbos@flooris.nl>
2022-10-11 08:46:48 +0200

    Fix missing (default) config for locale

--- config/ergonode.php
+++ config/ergonode.php
@@ -1,10 +1,11 @@
 <?php
 
 return [
-    'hostname' => env('ERGONODE_HOSTNAME', ''),
-    'username' => env('ERGONODE_USERNAME', ''),
-    'password' => env('ERGONODE_PASSWORD', ''),
+    'hostname'       => env('ERGONODE_HOSTNAME', ''),
+    'username'       => env('ERGONODE_USERNAME', ''),
+    'password'       => env('ERGONODE_PASSWORD', ''),
+    'locale'         => env('ERGONODE_LOCALE', "nl_NL"),
     'client-options' => [
-        'user-agent' => 'flooris/ergonode-api'
-    ]
+        'user-agent' => 'flooris/ergonode-api',
+    ],
 ];
